8cae6321e786bb52c9ce68f1298a2575b2625dfb
Bradley Teo <bradleyteowj@gmail.com>
2014-04-08 23:05:57 +0800

    Add LocalStorage Save & Load Functions

--- civdev/game.js
+++ civdev/game.js
@@ -74,13 +74,15 @@ var Game = (function (game) {
 	
 	game.eras = [
 		eraType("Paleolithic Era", "your civilization has begun to manufacture tools to aid in its development.", function () {return true;}),
-		eraType("Early Stone Age", "your civilization has discovered how to produce stone tools and has mastered the control of fire.", function () {return game.resources.stone > 0;})
+		eraType("Stone Age", "your civilization has discovered how to produce stone tools and has mastered the control of fire.", function () {return game.resources.stone > 0;})
 	];
 	
 	game.resourceTypes = [
 		resourceType("log", "a bit of a tree"),
 		resourceType("wood", "refined log"),
 		resourceType("charcoal", "burnt logs"),
+		resourceType("crude ore", "unrefined minerals"),
+		resourceType("iron", "the most versatile of metals")
 	];
 
 	game.conversionTypes = {
@@ -88,14 +90,18 @@ var Game = (function (game) {
 		"lumberjack": changeType("cuts logs", {'food': 20}, {'food': -4, 'log': 1}, ["wood gathering"]),
 		"woodworker": changeType("crafts logs into wood", {'food': 100, 'log': 200}, {'food': -2, 'log': -2, 'wood': 3}, ["craftsmanship"]),
 		"stone miner": changeType("mines stone", {'food': 150, 'log': 50, 'wood': 300}, {'food': -2, 'log': -2, 'wood': -2, 'stone': 1}, ["masonry"]),
+		"farmer": changeType("farms crops and animals", {'food': 150, 'log': 200, 'wood': 200}, {'food': 3, 'log': 2}, ["domestication", "agriculture"]),
+		
 	};
 	
 	game.researchTypes = {
-		"hunting": researchType("collect food", 0, [], 10),
-		"wood gathering": researchType("gather logs", 0, [], 30),
-		"craftsmanship": researchType("turn logs into wood", 0, ["wood gathering"], 50),
-		"masonry": researchType("work with stone", 0, ["craftsmanship"], 80),
-		"domestication": researchType("domesticate animals", 0, ["hunting", "wood gathering"], 100)
+		"hunting": researchType("stab things for meat", 0, [], 10),
+		"wood gathering": researchType("stab things for logs", 0, [], 30),
+		"craftsmanship": researchType("stab logs for wood", 0, ["wood gathering"], 50),
+		"masonry": researchType("stab stone", 0, ["craftsmanship"], 80),
+		"domestication": researchType("rear animals", 0, ["hunting", "wood gathering"], 100),
+		"agriculture": researchType("grow crops", 0, ["hunting", "wood gathering"], 120),
+		"mining": researchType("we require more minerals!", ["masonry"], 130)
 	};
 						
 	game.resources = {
@@ -124,21 +130,44 @@ var Game = (function (game) {
 	
 	game.researching = function (obj, researchType, progress) {
 		obj.find(".percentunlocked").text("" + Math.floor(progress * 100) + "%");
-		if (progress < 1)
+		if (progress < 1) {
+			game.researchProgress[researchType] = progress;
 			setTimeout(function () {game.researching(obj, researchType, progress + 0.01)}, game.researchTypes[researchType].time * 10 * game.researchMultiplier * game.currentlyResearching.length);
-		else {
+		} else {
 			game.researchProgress[researchType] = 1;
 			game.currentlyResearching.remove(game.currentlyResearching.indexOf(researchType));
 		}
 	};					
 	
+	var dataName = ["resources", "conversions", "researchProgress", "era", "researchMultiplier", "currentlyResearching", "researchedConversions"];
 	
+	game.load = function () {
+		var ls = localStorage.civdev_gameData;
+		if (!ls) return;
+		var jstr = atob(ls);
+		var gameData = JSON.parse(jstr);
+		
+		//Load Data
+		for (var i = 0; i < dataName.length; i++) {
+			game[dataName[i]] = gameData[dataName[i]] || game[dataName[i]];
+		}
+		
+	};
+	
+	game.save = function () {
+		var json = {};
+		for (var i = 0; i < dataName.length; i++) {
+			json[dataName[i]] = game[dataName[i]];
+		}
+		var jsonstr = JSON.stringify(json);
+		localStorage.civdev_gameData = btoa(jsonstr);
+	};
 						
-	var renderResearches = function () {
+	var renderResearches = function (era) {
 		var researchDiv = $("#researches");
 		for (var researchType in game.researchTypes) {
 			var curResearchType = game.researchTypes[researchType];
-			if (curResearchType.unlockedIn == game.era) {
+			if (curResearchType.unlockedIn == era) {
 				var curDiv = $("<div/>");
 				curDiv.addClass("research");
 				curDiv.data("researchType", researchType);
@@ -146,37 +175,57 @@ var Game = (function (game) {
 				curDiv.append($("<div class='researchdescription'>"+curResearchType.description+"</div>"));
 				if (curResearchType.requires.length > 0)
 					curDiv.append($("<div class='researchrequirements'>requires "+curResearchType.requires.join(", ")+"</div>"));
-				curDiv.append($("<div class='percentunlocked'>0%</div>"));
+				curDiv.append($("<div class='percentunlocked'>"+Math.floor((game.researchProgress[researchType] || 0) * 100)+"%</div>"));
 				researchDiv.append(curDiv);
-				curDiv.click(function () {
-					var type = $(this).data("researchType");
-					console.log(type + " clicked");
-					if (!game.researchProgress.hasOwnProperty(type)) {
-						for (var i = 0; i < game.researchTypes[type].requires.length; i++) {
-							if (game.researchProgress[game.researchTypes[type].requires[i]] != 1) return;
+				if ((game.researchProgress[curResearchType] || 0) == 0) {
+					curDiv.click(function () {
+						var type = $(this).data("researchType");
+						if (!game.researchProgress.hasOwnProperty(type)) {
+							for (var i = 0; i < game.researchTypes[type].requires.length; i++) {
+								if (game.researchProgress[game.researchTypes[type].requires[i]] != 1) return;
+							}
+							game.researchProgress[type] = 0;
+							game.currentlyResearching.push(type);
+							game.researching($(this), type, 0);
 						}
-						game.researchProgress[type] = 0;
-						game.currentlyResearching.push(type);
-						game.researching($(this), type, 0);
-					}
-				});
-				
+					});
+				}
 			}
 		}
 	};
+	
+	game.reload = function () {
+		game.era = 0;
+		game.speed = 1000;
+		game.researchMultiplier = 1.0;
+		game.resources = {
+		'food': 40
+		};
+		game.researchedConversions = [];
+		game.renderedConversions = [];
+		game.currentlyResearching = [];
+		game.researchProgress = {};
+	}
 						
 	game.init = function () {
-		renderResearches();
-	
+		for (var i = 0; i < game.era; i++) {
+			renderResearches(i);
+		}
+		for (var i = 0; i < game.currentlyResearching.length; i++) {
+			game.researching($(".research").filter(function(){return $(this).data("researchType") == game.currentlyResearching[i]}), game.currentlyResearching[i], game.researchProgress[game.currentlyResearching[i]]);
+		}
 		$(".research").click(function () {
 			
 		});
 	}
 	
 	game.renderLoop = function () {
+		//Render Eera
 		$("#era").text(game.eras[game.era].name);
 		$("#eradescription").text(game.eras[game.era].description);
 		document.title = game.eras[game.era].name;
+		
+		//Render Resources
 		var resourcesArr = [];
 		for (var resource in game.resources) {
 			resourcesArr.push([resource, game.resources[resource]]);
@@ -200,6 +249,7 @@ var Game = (function (game) {
 			if (game.researchedConversions.indexOf(conversion) != -1 && game.renderedConversions.indexOf(conversion) == -1) {
 				game.renderedConversions.push(conversion);
 				var productionDiv = $("<div/>");
+				productionDiv.data("production", conversion);
 				productionDiv.addClass("production");
 				productionDiv.append($("<div/>").addClass("productionname").text(conversion));
 				//productionDiv.append($("<div/>").addClass("productiondescription").text(curConversionType.description));
@@ -214,14 +264,14 @@ var Game = (function (game) {
 				}
 				
 				productionDiv.append($("<div/>").addClass("productioneffect").text(effects.join(", ")));
-				var addProductionDiv = $("<div/>").text("+").addClass("addproduction").data("production", conversion);
+				var addProductionDiv = $("<div/>").text("+").addClass("addproduction");
 				productionDiv.append(addProductionDiv);
-				var reduceProductionDiv = $("<div/>").text("-").addClass("reduceproduction").data("production", conversion);
+				var reduceProductionDiv = $("<div/>").text("-").addClass("reduceproduction");
 				productionDiv.append(reduceProductionDiv);
-				productionDiv.append($("<div/>").addClass("productionnumber").text("0"));
+				productionDiv.append($("<div/>").addClass("productionnumber").text(game.conversions[conversion] || 0));
 				productionsDiv.append(productionDiv);
 				addProductionDiv.click(function () {
-					var conversionType = $(this).data("production");
+					var conversionType = $(this).parent().data("production");
 					if (!game.conversions.hasOwnProperty(conversionType)) {
 						game.conversions[conversionType] = 0;
 					}
@@ -238,7 +288,7 @@ var Game = (function (game) {
 					$(this).parent().find(".productionnumber").text(game.conversions[conversionType]);
 				});
 				reduceProductionDiv.click(function () {
-					var conversionType = $(this).data("production");
+					var conversionType = $(this).parent().data("production");
 					if (!game.conversions.hasOwnProperty(conversionType)) {
 						game.conversions[conversionType] = 0;
 					}
@@ -248,6 +298,7 @@ var Game = (function (game) {
 			}
 		}
 		
+		game.save();
 		setTimeout(function () { game.renderLoop(); }, 100);
 		
 	}
@@ -258,7 +309,7 @@ var Game = (function (game) {
 		if (game.era < game.eras.length - 1) {
 			if (game.eras[game.era + 1].requirements()) {
 				game.era++;
-				renderResearches();
+				renderResearches(game.era);
 			}
 		}	
 
@@ -285,8 +336,10 @@ var Game = (function (game) {
 	return game;
 } (Game || {}));
 
-window.onload = function () {
+$(document).ready(function () {
+	
+	Game.load();
 	Game.init();
 	Game.renderLoop();
 	Game.loop();
-}
+});
