872e44a4baf97a8211d1d3c6419bac4342f765f2
btwj <bradleyteowj@gmail.com>
2018-11-07 23:12:01 +0800

    Change build path

---
+++ ord/precache-manifest.8685851fa040f6bfc11fddbe8b1af2e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "226fb276bceb0787b802",
+    "url": "/ord/static/js/main.226fb276.chunk.js"
+  },
+  {
+    "revision": "6212758421a1d6ac6930",
+    "url": "/ord/static/css/1.60247304.chunk.css"
+  },
+  {
+    "revision": "6212758421a1d6ac6930",
+    "url": "/ord/static/js/1.62127584.chunk.js"
+  },
+  {
+    "revision": "69896af82e191f64e11a",
+    "url": "/ord/static/js/runtime~main.69896af8.js"
+  },
+  {
+    "revision": "af5035d4226fd951b953fddde6c5da08",
+    "url": "/ord/index.html"
+  }
+];
