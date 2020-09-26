06e268418b91c0185052e91712b7fc16415dde50
Bradley Teo <bradleyteowj@gmail.com>
2020-09-27 04:14:27 +0800

    HackCMU 2020

---
+++ hackcmu/precache-manifest.642b4644ee1dcb68391182b1f7bf5d38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af7ad7b410e60d12bcaaf2c408d65ccc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f83174a61457d4b6d32f",
+    "url": "/static/css/main.3598422c.chunk.css"
+  },
+  {
+    "revision": "9f778b5ac203c3566699",
+    "url": "/static/js/2.5aadb0ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5aadb0ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f83174a61457d4b6d32f",
+    "url": "/static/js/main.79230918.chunk.js"
+  },
+  {
+    "revision": "c131d55fa3b832ac7601",
+    "url": "/static/js/runtime-main.58383973.js"
+  }
+]);
